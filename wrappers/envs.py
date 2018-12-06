92ed1ba3d099595861e1a1c7e1d15de92692f3d7
Seungjae Ryan Lee <seungjaeryanlee@gmail.com>
2018-12-06 01:29:31 +0000

    Add pseudocode for make_env

---
+++ wrappers/envs.py
@@ -0,0 +1,19 @@
+import gym
+
+
+def make_env(env_id):
+    """
+    Return an OpenAI Gym environment wrapped with appropriate wrappers.
+
+    Parameters
+    ----------
+    env_id : str
+        OpenAI Gym ID for environment.
+
+    Returns
+    -------
+    env
+        Wrapped OpenAI environment.
+
+    """
+    pass
