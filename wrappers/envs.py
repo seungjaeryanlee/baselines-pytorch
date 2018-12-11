44ba1561269376c37dcaab598e27c70cabb2670f
Seungjae Ryan Lee <seungjaeryanlee@gmail.com>
2018-12-11 05:05:37 +0000

    Support Super Mario Bros environment
    - Through gym-super-mario-bros package in PyPI
    - Deleted roms/ since it is not needed for this package: it was
      added for gym-retro

--- wrappers/envs.py
+++ wrappers/envs.py
@@ -1,6 +1,7 @@
 import gym
 
-from .atari_wrappers import make_atari, wrap_deepmind
+from .atari_wrappers import (make_atari, wrap_deepmind,
+                             ScaledFloatFrame, WarpFrame)
 from .torch_wrappers import TorchWrapper, AtariPermuteWrapper
 
 
@@ -26,6 +27,17 @@ def make_env(env_id):
     elif env_id == 'PongNoFrameskip-v4':
         env = make_atari(env_id)
         env = wrap_deepmind(env, frame_stack=True)
+        env = TorchWrapper(env)
+        env = AtariPermuteWrapper(env)
+    elif env_id == 'SuperMarioBros-v0':
+        from nes_py.wrappers import BinarySpaceToDiscreteSpaceEnv
+        import gym_super_mario_bros
+        from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
+
+        env = gym_super_mario_bros.make('SuperMarioBros-v0')
+        env = BinarySpaceToDiscreteSpaceEnv(env, SIMPLE_MOVEMENT)
+        env = WarpFrame(env)
+        env = ScaledFloatFrame(env)
         env = TorchWrapper(env)
         env = AtariPermuteWrapper(env)
     else:
